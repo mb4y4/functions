5dec8e889ba11ee709c065aeaad5cba26802a4f8
mb4y4 <117087528+mb4y4@users.noreply.github.com>
2023-09-02 12:03:00 +0300

    functions

---
+++ functions.js
@@ -0,0 +1,28 @@
+// function hi(name){
+//     console.log(`Hi, ${name}`);
+// }
+
+// hi('Denis');
+
+
+// const sayHello = (name) => {
+//     console.log("hello " + name);
+//     }
+//     sayHello('Denis');
+
+
+// const addition = function(a, b) {
+//     return a + b;
+// }
+
+// console.log(`${addition(20 , 3)}`);
+
+
+// function factorial(n){
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//         }
+// }
+// console.log(`${factorial(5)}`);
